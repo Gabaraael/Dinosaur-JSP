14669f21ce71e2a5ba1e43441be320351391040c
gabaraael <gabriel1998.gp45@gmail.com>
2023-03-08 22:36:42 -0400

    Criando model e dao

--- meooooooooooooooooooooooooow/src/main/java/br/edu/ifms/controller/IndexController.java
+++ meooooooooooooooooooooooooow/src/main/java/br/edu/ifms/controller/IndexController.java
@@ -1,7 +1,8 @@
-package br.edu.ifms.Controller;
+package br.edu.ifms.controller;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.edu.ifms.dao.util.Conexao;
+import br.edu.ifms.model.Usuario;
 
 /**
  * Servlet implementation class IndexController
@@ -19,11 +21,12 @@ import br.edu.ifms.dao.util.Conexao;
 public class IndexController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public IndexController() {
-		super();
-	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processaRequisicao(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		processaRequisicao(request, response);
 	}
@@ -36,21 +39,22 @@ public class IndexController extends HttpServlet {
 			case "novo":
 				novoUsuario(request, response);
 				break;
+			case "inserir":
+				inserirNovoUsuario(request, response);
+				break;				
 			}
 		} catch (Exception ex) {
 			throw new ServletException(ex);
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		processaRequisicao(request, response);
-	}
+	
 
 	private void novoUsuario(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		Connection onePunchConnection = Conexao.getConexao();
+		
 		if (onePunchConnection != null) {
 			System.out.println("Capturei uma connection, meu primeiro pokennection");
 		} else {
@@ -59,6 +63,29 @@ public class IndexController extends HttpServlet {
 
 		RequestDispatcher dispatcher = request.getRequestDispatcher("publica/publicacaoUsuario.jsp");
 		dispatcher.forward(request, response);
+	}
+	
+	private void inserirNovoUsuario(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+				
+		String nome = request.getParameter("nome");
+		String cpf = request.getParameter("cpf");
+		String dataNascimento = request.getParameter("dataNascimento");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String login = request.getParameter("login");
+		String ativo = request.getParameter("ativo");	
+		
+//		Usuario usuario = new Usuario(nome, cpf, dataNascimento, email, password, login, ativo);
+		
+		
+		Connection onePunchConnection = Conexao.getConexao();
+		
+		if (onePunchConnection != null) {
+			System.out.println("Capturei uma inserir connection, meu primeiro pokennection");
+		} else {
+
+		}
 	}
 
 }
