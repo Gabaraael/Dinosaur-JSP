b3479a5c35b0237f4d939555bc0a946013f2d2c0
gabaraael <gabriel1998.gp45@gmail.com>
2023-03-22 22:10:23 -0400

    Adicionando servidor tomcat

--- meooooooooooooooooooooooooow/src/main/java/br/edu/ifms/controller/IndexController.java
+++ meooooooooooooooooooooooooow/src/main/java/br/edu/ifms/controller/IndexController.java
@@ -2,7 +2,11 @@ package br.edu.ifms.controller;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.util.HashMap;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import br.edu.ifms.dao.util.Conexao;
 import br.edu.ifms.model.Usuario;
+import br.edu.ifms.util.ManipulacaoData;
 
 /**
  * Servlet implementation class IndexController
@@ -21,6 +26,8 @@ import br.edu.ifms.model.Usuario;
 public class IndexController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private Connection connection;
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		processaRequisicao(request, response);
@@ -65,9 +72,25 @@ public class IndexController extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 	
+	private void conectar() throws SQLException {
+		if (connection == null || connection.isClosed()) {
+			connection = Conexao.getConexao();
+		}
+	}
+	
+	private void desconectar() throws SQLException {
+		if (connection != null && !connection.isClosed()) {
+			connection.close();
+		}
+	}
+	
 	private void inserirNovoUsuario(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-				
+			throws SQLException  {
+		String sql = "INSERT INTO usuario (nome, cpf, data_nascimento, email, password, login, ativo)"
+				+ " VALUES (?, ?, ?, ?, ?, ?, ?)";	
+
+		//		Connection onePunchConnection = Conexao.getConexao();
+		
 		String nome = request.getParameter("nome");
 		String cpf = request.getParameter("cpf");
 		String dataNascimento = request.getParameter("dataNascimento");
@@ -76,10 +99,52 @@ public class IndexController extends HttpServlet {
 		String login = request.getParameter("login");
 		String ativo = request.getParameter("ativo");	
 		
+		conectar();
+		
+		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		
+		statement.setString(1,nome);
+		statement.setString(2, cpf);
+		String dataNumero = dataNascimento;		
+		ManipulacaoData manipulacaoData = new ManipulacaoData();
+		Date data = manipulacaoData.convertStringToDate(dataNumero);		
+		statement.setDate(3, data);
+		statement.setString(4, email);
+		statement.setString(5, password);
+		statement.setString(6, login);
+		statement.setBoolean(7, ativo);
+		
+		statement.executeUpdate();
+		
+		ResultSet resultSet = statement.getGeneratedKeys();
+		long id = 0;
+		if(resultSet.next())
+			id = resultSet.getInt("id");
+		statement.close();
+
+		desconectar();
+		
+		//
+		
+//		statement.setString(1, usuario.getNome());
+//		statement.setString(2, usuario.getCpf());
+//		long dataNumero = usuario.getDataNascimento().getTime(); 
+//		Date nascimento = new Date(dataNumero);
+//		statement.setDate(3, nascimento);
+//		statement.setString(4, usuario.getEmail());
+//		statement.setString(5, usuario.getPassword());
+//		statement.setString(6, usuario.getLogin());
+//		statement.setBoolean(7, usuario.isAtivo());
+		
+//		usuario.setId(id);
+//		return usuario;
+//				
+	
+		
 //		Usuario usuario = new Usuario(nome, cpf, dataNascimento, email, password, login, ativo);
 		
 		
-		Connection onePunchConnection = Conexao.getConexao();
+		
 		
 		if (onePunchConnection != null) {
 			System.out.println("Capturei uma inserir connection, meu primeiro pokennection");
